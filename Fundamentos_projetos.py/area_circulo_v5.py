9a2304e961029e51e7016d54ed6e0b29e6c50c8c
Geraud <geraud.pereira@hotmail.com>
2023-11-11 11:06:11 -0300

    Rascunho.projeto

---
+++ Fundamentos_projetos.py/area_circulo_v5.py
@@ -0,0 +1,8 @@
+#! python
+from math import pi
+
+# print (dir())
+
+raio = 15.3
+print ("Area do circulo", pi * raio ** 2)
+ 
--- area_circulo_v4.py
+++
@@ -1,6 +0,0 @@
-#! python
-import math
-
-raio = 15.3
-print ("Area do circulo", math.pi * raio ** 2)
- 
