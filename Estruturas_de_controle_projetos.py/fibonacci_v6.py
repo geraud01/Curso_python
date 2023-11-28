a5e460b2940564d84441c897981c1a58c5db629f
Geraud <geraud.pereira@hotmail.com>
2023-11-28 13:54:29 -0300

    fibonacci

--- Estruturas_de_controle_projetos.py/fibonacci_v6.py
+++ Estruturas_de_controle_projetos.py/fibonacci_v6.py
@@ -1,10 +1,11 @@
-def fibonacci(limite):
+def fibonacci(quantidade):
     resultado = [0, 1]
-    while resultado[-1]  < limite:
+    while True:
         resultado.append( sum(resultado[-2:]))
+        if len(resultado) == quantidade:
+            break
     return resultado
 
-
 if __name__ == '__main__':
-    for fib in fibonacci(10000):
+    for fib in fibonacci(20):
         print(fib)
