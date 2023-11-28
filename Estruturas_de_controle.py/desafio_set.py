03ec46e2950e205d95634422209afce9c5e67f81
Geraud <geraud.pereira@hotmail.com>
2023-11-28 12:39:08 -0300

    exercicio

---
+++ Estruturas_de_controle.py/desafio_set.py
@@ -0,0 +1,13 @@
+PALAVRAS_PROIBIDAS = {'Futebol', 'religião', 'politica'}
+textos = [
+    'Joao gosta de futebol e politica',
+    'A praia foi divertida'
+]
+
+for texto in textos:
+    intersecao = PALAVRAS_PROIBIDAS.intersection(set(texto.lower().split()))
+    if intersecao:
+        print ("Texto possui palavras proibidas:", intersecao)
+    else:
+        print("Texto autorizado:", texto)
+        
